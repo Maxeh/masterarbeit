dbc84c3c4968ca9562735a0f0c6a1f27bbcf1966
Maximilian <ehbauermax@yahoo.de>
2018-06-19 15:20:40 +0200

    Recycler View added to NewsTab

--- AndroidSDK/app/src/main/java/maxeh/androidsdk/masternews/MainActivity.java
+++ AndroidSDK/app/src/main/java/maxeh/androidsdk/masternews/MainActivity.java
@@ -1,7 +1,5 @@
 package maxeh.androidsdk.masternews;
 
-
-import android.support.design.internal.NavigationMenuView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
@@ -12,6 +10,7 @@ import android.support.design.widget.TabLayout;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
--- AndroidSDK/app/src/main/java/maxeh/androidsdk/masternews/NewsTab.java
+++ AndroidSDK/app/src/main/java/maxeh/androidsdk/masternews/NewsTab.java
@@ -3,27 +3,70 @@ package maxeh.androidsdk.masternews;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class NewsTab extends Fragment {
+public class NewsTab extends Fragment implements NewsTabRecyclerAdapter.ItemClickListener {
 
+    NewsTabRecyclerAdapter mAdapter;
+    View mView;
 
-    public NewsTab() {
-        // Required empty public constructor
-    }
-
+    public NewsTab() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_news_tab, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        mView = view;
+
+        // data to populate the RecyclerView with
+        ArrayList<String> animalNames = new ArrayList<>();
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = mView.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mAdapter = new NewsTabRecyclerAdapter(getActivity(), animalNames);
+        mAdapter.setClickListener(this);
+        recyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(getActivity(), "You clicked " + mAdapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 
 }
